54636e2d0864255b91b90a5641a78f3cc12a0eb2
jossamador <jos-amador@hotmail.com>
2025-10-14 15:28:32 -0600

    Se agregaron las configuraciones de rutas en app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PlaceListComponent } from './pages/place-list/place-list.component';
+import { TrailListComponent } from './pages/trail-list/trail-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inicio', component: HomeComponent },
+  { path: 'lugares', component: PlaceListComponent },
+  { path: 'senderos', component: TrailListComponent },
+  // Ruta por defecto: si la URL está vacía, redirige a /inicio
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  // Ruta comodín: si la URL no coincide con ninguna, redirige a /inicio
+  { path: '**', redirectTo: '/inicio' }
+];
