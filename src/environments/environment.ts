367e685cf7a805848b61c6428610beda410f30b0
jossamador <jos-amador@hotmail.com>
2025-11-26 12:13:00 -0600

    Se actualizó url para desplegar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   mapboxToken: 'pk.eyJ1Ijoiam9zc2FtYWRvciIsImEiOiJjbWdyNDAwYnYwYzJnMmpwbjJqM3ZoaDZ3In0.XF04bFG6nzH-2vmtR4tG-g',
   apiUrl: 'https://mexnatureapi-production.up.railway.app/api',
 };
